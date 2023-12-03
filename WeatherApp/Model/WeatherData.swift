d5125034d60615db5cc06aec085570beca1b450a
AlexiaSaucedo <AlexiaSaucedo@users.noreply.github.com>
2023-12-02 21:26:24 -0600

    Model: Weather Data.swift

---
+++ WeatherApp/Model/WeatherData.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherData.swift
+//  WeatherApp
+//
+//  Created by Alexia Fernanda Saucedo Romero on 02/12/23.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let location: Location
+    let current: Current
+}
+
+struct Current : Codable {
+    let temperature: Int
+    let weather_code: Int
+}
+
+struct Location: Codable {
+    let name: String
+}
